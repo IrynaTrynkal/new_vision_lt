5101abfe537ab0db856c5f24bbd4691b9527bd3d
IraMira333 <irinat333@gmail.com>
2026-01-22 08:52:11 +0200

    Update HeroDoctors.tsx

--- src/components/pageDoctors/HeroDoctors.tsx
+++ src/components/pageDoctors/HeroDoctors.tsx
@@ -5,7 +5,7 @@ import { Fraction } from "../shared/Fraction";
 export const HeroDoctors = () => {
     const t = useTranslations("Doctors");
     return (
-        <section className="tab:mt-0 tab:h-120 prepc:h-[552px] relative mt-12 h-[calc(60vh-48px)] bg-[url(/images/doctors-hero1.jpg)] bg-cover bg-right-top bg-no-repeat">
+        <section className="tab:h-120 prepc:h-[552px] relative mt-12 h-[calc(60vh-48px)] bg-[url(/images/doctors-hero1.jpg)] bg-cover bg-right-top bg-no-repeat">
             <div className="prepc:hidden about-hero-gradient absolute bottom-0 z-0 h-full w-full" />
             <div className="services-hero-big prepc:block absolute bottom-0 z-0 hidden h-full w-full" />
             <div className="content tab:pt-20 tab:pb-10 prepc:pt-[104px] prepc:pb-[60px] relative z-[1] flex h-full flex-col justify-between pt-7 pb-6">
